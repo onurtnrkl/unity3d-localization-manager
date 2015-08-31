2f642a6ba367475c0b45023516bcb2a551bb1468
Onur Tanrıkulu <onurtnrkl@gmail.com>
2015-08-31 19:23:07 +0300

    Update

---
+++ Example/Assets/Scripts/Example.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Example : MonoBehaviour
+{
+    public Text fruit1;
+    public Text fruit2;
+    public Text fruit3;
+
+	void Start ()
+    {
+        SetText();
+    }
+
+    public void OnClick(string language)
+    {
+        LanguageManager.Instance.LoadLanguage(language);
+        SetText();
+    }
+
+    private void SetText()
+    {
+        fruit1.text = LanguageManager.Instance.GetString("Fruits/Apple");
+        fruit2.text = LanguageManager.Instance.GetString("Fruits/Raspberry");
+        fruit3.text = LanguageManager.Instance.GetString("Fruits/Banana");
+    }
+}
