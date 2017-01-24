7c39949665dbb0a07bae552c70d53caad100de9d
Onur Tanrikulu <onurtnrkl@gmail.com>
2017-01-24 21:11:27 +0300

    Localization Editor added

---
+++ Editor/LocalizationEditor.cs
@@ -0,0 +1,103 @@
+/*================================================================
+Product:    LocalizationManager
+Developer:  Onur Tanrıkulu
+Company:    Onur Tanrikulu
+Date:       24/01/2017 17:40
+
+Copyright (c) 2017 Onur Tanrikulu. All rights reserved.
+================================================================*/
+
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class LocalizationEditor : EditorWindow
+{
+   #region Fields
+    private static string localizationDirectory;
+    private string languageCode;
+    public LocalizationData localizationData;
+   #endregion
+ 
+   #region Methods
+    [MenuItem("Window/Localization Editor")]
+    private static void Inıt()
+    {
+        EditorWindow.GetWindow(typeof(LocalizationEditor)).Show();
+
+        localizationDirectory = Path.Combine(Application.streamingAssetsPath, "Localization");
+
+        if (!Directory.Exists(localizationDirectory))
+            {
+                Directory.CreateDirectory(localizationDirectory);
+            }
+    }
+
+    private void OnGUI()
+    {
+        languageCode = EditorGUILayout.TextField("Language Code", languageCode);
+
+        if (localizationData != null)
+        {
+            SerializedObject serializedObject = new SerializedObject(this);
+            SerializedProperty serializedProperty = serializedObject.FindProperty("localizationData");
+            EditorGUILayout.PropertyField(serializedProperty, true);
+            serializedObject.ApplyModifiedProperties();
+
+            GUILayout.FlexibleSpace();
+
+            if (GUILayout.Button("Save", GUILayout.Height(50)))
+            {
+                SaveData();
+            } 
+        }
+        else
+        {
+            GUILayout.FlexibleSpace();
+        }
+            
+        if (GUILayout.Button ("Load", GUILayout.Height(50))) 
+        {
+            LoadData ();
+        }
+
+        if (GUILayout.Button ("Create", GUILayout.Height(50)))
+        {
+            CreateNewData ();
+        }
+    }
+
+    private void LoadData()
+    {
+        string filePath = EditorUtility.OpenFilePanel ("Select localization data file", localizationDirectory, "json");
+
+        if (!string.IsNullOrEmpty (filePath)) 
+        {
+            string jsonData = File.ReadAllText (filePath);
+
+            languageCode = Path.GetFileNameWithoutExtension(filePath);
+            localizationData = JsonUtility.FromJson<LocalizationData> (jsonData);
+        }
+    }
+
+    private void SaveData()
+    {
+        string fileName = languageCode + ".json";
+        string filePath = Path.Combine(localizationDirectory, fileName);
+
+        if (!string.IsNullOrEmpty(filePath))
+        {
+            string jsonData = JsonUtility.ToJson(localizationData);
+            File.WriteAllText(filePath, jsonData);
+            AssetDatabase.Refresh();
+        }
+    }
+
+    private void CreateNewData()
+    {
+        localizationData = new LocalizationData ();
+    }
+   #endregion
+}
+
+
---
+++ LocalizationData.cs
@@ -0,0 +1,23 @@
+/*================================================================
+Product:    LocalizationManager
+Developer:  Onur Tanrıkulu
+Company:    Onur Tanrikulu
+Date:       24/01/2017 17:17
+
+Copyright (c) 2017 Onur Tanrikulu. All rights reserved.
+================================================================*/
+
+using System;
+
+[Serializable]
+public class LocalizationData
+{
+    public LocalizationItem[] items;
+}
+
+[Serializable]
+public class LocalizationItem
+{
+    public string key;
+    public string text;
+}
